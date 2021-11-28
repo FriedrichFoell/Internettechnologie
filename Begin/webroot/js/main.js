3f4480997df482123ac2147d2fdc5cffc12dc108
Friedrich <s44417@bth-berlin.de>
2021-11-28 13:36:05 +0100

    Version2
    Version 2

--- Begin/webroot/js/main.js
+++ Begin/webroot/js/main.js
@@ -1,2 +1,16 @@
-// Nur zum Test
-//alert("Willkommen");
+function KommentarKnopf(Kommentarfeld){
+
+              document.getElementById(Kommentarfeld).style.display = 'block';
+
+  }
+  function countChars(Kommentarfeld){
+      var maxLength = 500;
+      var strLength = Kommentarfeld.value.length;
+      var charRemain = (maxLength - strLength);
+
+      if(charRemain <= 0){
+          document.getElementById("Zeichen").innerHTML = '<span style="color: red;">Sie haben die maximale Anzahl an Zeichen erreicht</span>';
+      }else{
+          document.getElementById("Zeichen").innerHTML = charRemain+' characters remaining';
+      }
+  }
